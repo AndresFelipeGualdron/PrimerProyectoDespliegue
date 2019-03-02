6a861d79993940df5ba5bdde973a350df1e93721
unknown <2128884@labinfo.is.escuelaing.edu.co>
2019-03-02 09:28:49 -0500

    intetando corregir

---
+++ src/test/java/edu/eci/cvds/prueba/test1.java
@@ -0,0 +1,23 @@
+package edu.eci.cvds.prueba;
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class test1 
+    extends TestCase
+{
+    
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
